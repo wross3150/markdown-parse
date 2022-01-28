edf87ee402712a954e88c9c12be7ee63ebdc9381
wross3150 <81711875+wross3150@users.noreply.github.com>
2022-01-27 18:52:26 -0800

    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,8 @@
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+// javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,13 +14,21 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        if (markdown.contains("[") && markdown.contains("]") && markdown.contains("(") && markdown.contains(")")) {
+            while(currentIndex < markdown.length()) {
+                int nextOpenBracket = markdown.indexOf("[", currentIndex);
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+                    currentIndex++;
+                    continue;
+                }
+                if (!(markdown.charAt(nextOpenBracket-1) == '!')) {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
+                currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
---
+++ test-file-new.md
@@ -0,0 +1,1 @@
+[a link to google docs](https://docs.google.com/document/d/1t1bUO0TSyzCIg1D3FT1M63beETcMIQX_jSEnCth171k/edit#)
